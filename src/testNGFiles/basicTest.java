c6ef2494da21cea86e25385b38f85c514f7a0eff
udit <mrudit26@gmail.com>
2018-08-24 09:36:18 +0530

    //second commit

---
+++ src/testNGFiles/basicTest.java
@@ -0,0 +1,14 @@
+package testNGFiles;
+
+import org.testng.annotations.Test;
+
+public class basicTest {
+	
+	@Test
+	public void BasicTest1()
+	{
+		System.out.println("this is basic test");
+	}
+	
+
+}
